31d1863b542ab71858265e5d04fe2145c75fae6f
Shougot mollik <shougotmollik@gmail.com>
2024-04-04 00:32:07 +0600

    outline button  theme

---
+++ lib/utils/theme/custom_theme/outlined_button_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppOutlinedButtonTheme {
+  AppOutlinedButtonTheme._();
+
+  // light Outlined Button
+  static final lightOutlinedButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.black,
+      side: const BorderSide(color: Colors.deepOrange),
+      textStyle: const TextStyle(
+          fontSize: 16, color: Colors.black, fontWeight: FontWeight.w600),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    ),
+  );
+
+  // dark Outline Button
+  static final darkOutlinedButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.black,
+      side: const BorderSide(color: Colors.deepOrangeAccent),
+      textStyle: const TextStyle(
+          fontSize: 16, color: Colors.black, fontWeight: FontWeight.w600),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    ),
+  );
+}
