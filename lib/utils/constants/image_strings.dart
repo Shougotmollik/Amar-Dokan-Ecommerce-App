7eb7e38eb9561ea1aa9b998ccd19831c93b376ca
Shougot mollik <shougotmollik@gmail.com>
2024-04-04 12:43:34 +0600

    image

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+class AppImageStrings {
+  AppImageStrings._();
+
+//   App Splash Screen
+  static const String appSplashScreen =
+      "assets/logos/amarDokanSplashScreen.png";
+
+//   Social logos
+  static const String facebook = "assets/logos/facebook.png";
+  static const String google = "assets/logos/google.png";
+}
