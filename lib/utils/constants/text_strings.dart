d95f0e89f5ccb48ae87e996b8dca71e477d83299
Shougot mollik <shougotmollik@gmail.com>
2024-04-04 13:03:23 +0600

    text string

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTextStrings {
+  AppTextStrings._();
+
+//Global Text
+
+//onBoardingText
+
+  static const String onBoardingTitle1 = "Choose Your Product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at Your door step";
+
+  static const String onBoardingSubTitle1 =
+      "Welcome to a world of limitless choices - Your perfect product await!";
+  static const String onBoardingSubTitle2 =
+      "For seamless transactions, choose your payment path - Your convenience, our priority";
+  static const String onBoardingSubTitle3 =
+      "From our door steps to yours - swifts secure , and contactless Delivery!";
+
+//   Home
+  static const String homeAppbarTitle = "Good day for Shopping";
+  static const String homeAppbarSubTitle = "Shougot Mollik";
+}
