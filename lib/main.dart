b96ec6b2ab5fc201c4b95b8781dc4e57b5b9e846
Shougot mollik <shougotmollik@gmail.com>
2024-04-04 17:03:53 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
-import 'package:ecommerce_app_project/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'myapp.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-    );
-  }
 }
