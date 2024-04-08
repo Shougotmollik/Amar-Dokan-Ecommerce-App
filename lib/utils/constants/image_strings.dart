ad637640b032a1035c73a01408aa183ba2ad539c
Shougot mollik <shougotmollik@gmail.com>
2024-04-08 10:03:54 +0600

    updated

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -8,4 +8,12 @@ class AppImageStrings {
 //   Social logos
   static const String facebook = "assets/logos/facebook.png";
   static const String google = "assets/logos/google.png";
+
+//   OnboardingImages
+  static const String onboarding1 =
+      "assets/images/on_boarding_images/choose.gif";
+  static const String onboarding2 =
+      "assets/images/on_boarding_images/payment.gif";
+  static const String onboarding3 =
+      "assets/images/on_boarding_images/delivery.gif";
 }
