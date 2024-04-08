17f7b5779e2d048f820ada432b2c543964281588
Shougot mollik <shougotmollik@gmail.com>
2024-04-08 10:03:22 +0600

    updated

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,11 +1,11 @@
-import 'package:ecommerce_app_project/utils/theme/custom_theme/appbar_theme.dart';
-import 'package:ecommerce_app_project/utils/theme/custom_theme/bottom_sheet_theme.dart';
-import 'package:ecommerce_app_project/utils/theme/custom_theme/checkbox_theme.dart';
-import 'package:ecommerce_app_project/utils/theme/custom_theme/chip_theme.dart';
-import 'package:ecommerce_app_project/utils/theme/custom_theme/elevated_button_theme.dart';
-import 'package:ecommerce_app_project/utils/theme/custom_theme/outlined_button_theme.dart';
-import 'package:ecommerce_app_project/utils/theme/custom_theme/text_field_decoration_theme.dart';
-import 'package:ecommerce_app_project/utils/theme/custom_theme/text_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/appbar_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/bottom_sheet_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/checkbox_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/chip_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/elevated_button_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/outlined_button_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/text_field_decoration_theme.dart';
+import 'package:amardokan/utils/theme/custom_theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
